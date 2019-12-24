7bb903a840b4471ffdc8a0e2eb3cd86aef84cedd
johnmcraig <jmcraig32884@gmail.com>
2019-12-23 23:51:16 -0500

    Updated Api to dotnet core 3.1

--- src/MeetingEvents/Api/Controllers/ValuesController.cs
+++ src/MeetingEvents/Api/Controllers/ValuesController.cs
@@ -1,45 +1,53 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers
+namespace Api.Controllers 
 {
-    [Route("api/[controller]")]
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ValuesController : ControllerBase 
     {
+        public ValuesController() 
+        { 
+
+        }
+
         // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        [HttpGet ("")]
+        public ActionResult<IEnumerable<string>> Getstrings() 
         {
-            return new string[] { "value1", "value2" };
+            return new List<string> { };
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [HttpGet ("{id}")]
+        public ActionResult<string> GetstringById(int id)
         {
-            return "value";
+            return null;
         }
 
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        [HttpPost ("")]
+        public void Poststring(string value) 
+        { 
+
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
+        [HttpPut ("{id}")]
+        public void Putstring(int id, string value) 
+        { 
+
         }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        [HttpDelete ("{id}")]
+        public void DeletestringById(int id)
+        { 
+
         }
     }
 }
