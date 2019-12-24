4eef46c1ab1371eb8037255136e6fe1626d51dcc
johnmcraig <jmcraig32884@gmail.com>
2019-12-23 23:54:39 -0500

    Small format change

--- src/MeetingEvents/Api/Controllers/ValuesController.cs
+++ src/MeetingEvents/Api/Controllers/ValuesController.cs
@@ -16,35 +16,35 @@ namespace Api.Controllers
         }
 
         // GET api/values
-        [HttpGet ("")]
+        [HttpGet("")]
         public ActionResult<IEnumerable<string>> Getstrings() 
         {
             return new List<string> { };
         }
 
         // GET api/values/5
-        [HttpGet ("{id}")]
+        [HttpGet("{id}")]
         public ActionResult<string> GetstringById(int id)
         {
             return null;
         }
 
         // POST api/values
-        [HttpPost ("")]
+        [HttpPost("")]
         public void Poststring(string value) 
         { 
 
         }
 
         // PUT api/values/5
-        [HttpPut ("{id}")]
+        [HttpPut("{id}")]
         public void Putstring(int id, string value) 
         { 
 
         }
 
         // DELETE api/values/5
-        [HttpDelete ("{id}")]
+        [HttpDelete("{id}")]
         public void DeletestringById(int id)
         { 
 
