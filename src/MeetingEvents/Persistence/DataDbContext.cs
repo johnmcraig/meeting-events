5faabba05988100a7c434d35d7585936d4ea4e93
johnmcraig <jmcraig32884@gmail.com>
2019-12-23 23:53:16 -0500

    Renamed default class to context and briught in Ef Core

---
+++ src/MeetingEvents/Persistence/DataDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base (options)
+        {
+            
+        }
+    }
+}
