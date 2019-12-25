739e6c960b012b31e41517557a1ca303ff5cc9aa
johnmcraig <jmcraig32884@gmail.com>
2019-12-25 11:28:16 -0500

    Cleaned up default comments

--- src/MeetingEvents/Api/Startup.cs
+++ src/MeetingEvents/Api/Startup.cs
@@ -24,7 +24,6 @@ namespace Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataDbContext>(opt => 
@@ -35,7 +34,6 @@ namespace Api
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
