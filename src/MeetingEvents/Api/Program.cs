eadeee0d42365d9d2dfcb59d6215185da3f77576
johnmcraig <jmcraig32884@gmail.com>
2019-12-25 11:15:27 -0500

    Setup host builder to initialize database, migrate, and create it

--- src/MeetingEvents/Api/Program.cs
+++ src/MeetingEvents/Api/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace Api
 {
@@ -13,7 +16,25 @@ namespace Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+           
+           using(var scope = host.Services.CreateScope())
+           {
+               var services = scope.ServiceProvider;
+
+               try
+               {
+                    var dbContext = services.GetRequiredService<DataDbContext>();
+                    dbContext.Database.Migrate();
+               }
+               catch(Exception ex)
+               {
+                   var logger = services.GetRequiredService<ILogger<Program>>();
+                   logger.LogError($"Could not create or migrate database, see full error: {ex}");
+               }
+           }
+
+           host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
