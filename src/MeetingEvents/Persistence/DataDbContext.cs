309540ebc8b30e7735a6f800f02ae3a6beafd40c
johnmcraig <jmcraig32884@gmail.com>
2019-12-27 13:54:57 -0500

    Added seed data in context and new migrations

--- src/MeetingEvents/Persistence/DataDbContext.cs
+++ src/MeetingEvents/Persistence/DataDbContext.cs
@@ -16,9 +16,14 @@ namespace Persistence
 
         public DbSet<Value> Values { get; set; }
 
-        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        // {
-        //     optionsBuilder.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-        // }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value { Id = 1, Name = "Value 101", Description = "A value with property of 101"},
+                new Value { Id = 2, Name = "Value 102", Description = "A value with property of 102"},
+                new Value { Id = 3, Name = "Value 103", Description = "A value with property of 103"}
+            );
+        }
     }
 }
