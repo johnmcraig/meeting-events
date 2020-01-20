364ded04bc1fc214bb5803914311ac6ab41fd6c2
johnmcraig <jmcraig32884@gmail.com>
2020-01-20 18:19:12 -0500

    Improved Edit class methods

--- src/MeetingEvents/Application/Activities/Edit.cs
+++ src/MeetingEvents/Application/Activities/Edit.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
+using Domain;
 using Persistence;
+using MediatR;
 
 namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest { }
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime? Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
 
         public class Handler : IRequestHandler<Command>
         {
@@ -22,7 +32,17 @@ namespace Application.Activities
             public async Task<Unit> Handle(Command request,
             CancellationToken cancellationToken)
             {
-                // handler logic
+                var activity = await _dbContext.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    throw new Exception("Could not find activity");
+
+                activity.Title = request.Title ?? activity.Title;
+                activity.Description = request.Description ?? activity.Description;
+                activity.Category = request.Category ?? activity.Category;
+                activity.Date = request.Date ?? activity.Date;
+                activity.City = request.City ?? activity.City;
+                activity.Venue = request.Venue ?? activity.Venue;
 
                 var success = await _dbContext.SaveChangesAsync() > 0;
 
