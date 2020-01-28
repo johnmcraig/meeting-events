4a5c528a0340503ddcd7d84a66dfc973666590fc
johnmcraig <jmcraig32884@gmail.com>
2020-01-28 00:09:41 -0500

    Refactored list element out and imported list component

--- src/MeetingFinder/client-app/src/features/meeting/MeetingDashboard.tsx
+++ src/MeetingFinder/client-app/src/features/meeting/MeetingDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Grid, List } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import { IActivity } from '../../app/models/activity'
+import { MeetingList } from './MeetingList'
 
 interface IProps {
     activities: IActivity[]
@@ -10,13 +11,14 @@ const MeetingDashboard: React.FC<IProps> = ({ activities }) => {
     return (
         <Grid>
             <Grid.Column width={10}>
-                <List>
+                <MeetingList activities={activities} />
+                {/* <List>
                     {activities.map((activity: any) => (
                         <List.Item key={activity.id}>
                             {activity.title}
                         </List.Item>
                     ))}
-                </List>
+                </List> */}
             </Grid.Column>
         </Grid>
     )
