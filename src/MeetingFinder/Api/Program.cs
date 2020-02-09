606d70cff3420522a7545c7362e68272a7e9148d
johnmcraig <jmcraig32884@gmail.com>
2020-02-08 23:51:31 -0500

    Updated main method to seed users on startup

--- src/MeetingFinder/Api/Program.cs
+++ src/MeetingFinder/Api/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,8 +23,9 @@ namespace Api
                try
                {
                     var dbContext = services.GetRequiredService<DataDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
                     dbContext.Database.Migrate();
-                    Seed.SeedData(dbContext);
+                    Seed.SeedData(dbContext, userManager).Wait();
                }
                catch(Exception ex)
                {
